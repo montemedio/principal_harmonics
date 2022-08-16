4cc55d5fe74c99c46ad1dc88d1071c9403b0621a
Lukas Middelberg <lukas@montemedio.de>
2022-08-16 14:42:56 +0200

    Fix dependencies

--- setup.py
+++ setup.py
@@ -7,5 +7,10 @@ setup(
     author='Lukas Middelberg',
     author_email='lukas@montemedio.de',
     packages=['principal_harmonics'],
-    install_requires=['pya', 'librosa', 'matplotlib', 'scipy', 'numpy', 'mido', 'sklearn'],
+    install_requires=['pya', 'librosa', 'matplotlib', 'scipy', 'numpy', 'mido', 'sklearn', 'ipympl', 'pandas', 'tqdm',
+
+        # For some reason pip complains that these packages are not installed...
+        # They seem to be some kind of transitive dependency.
+        'zlmdb', 'treq'
+    ],
 )
